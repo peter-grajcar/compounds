0549933d2403df2707d272f738de6c818638a342
petergrajcar <peter.grajcar131@gmail.com>
2020-04-17 15:47:41 +0200

    implement element name stemming

--- compounds.hs
+++ compounds.hs
@@ -57,9 +57,18 @@ data Compound = Atom String Integer | Group [Compound] Integer
 instance Show Compound where
     show (Atom a n) = "(" ++ a ++ (show n) ++ ")"
     show (Group cs n)
-        | n == 1    = concat [show c | c <- cs]
-        | otherwise = "(" ++ concat [show c | c <- cs] ++ ")" ++ (show n)
+        | n == 1    = "(" ++ concat [show c | c <- cs] ++ ")" 
+        | otherwise = "(" ++ concat [show c | c <- cs] ++ (show n) ++ ")" 
 
+-- >>> parse [AtomToken "H", NumberToken 2, AtomToken "O", NumberToken 1]
+-- ((H2)(O1))
+--
+-- >>> parse [AtomToken "Na", LParenToken, AtomToken "O", AtomToken "H", RParenToken, NumberToken 2]
+-- ((Na1)((O1)(H1)2))
+--
+-- >>> parse [AtomToken "H", NumberToken 2, AtomToken "S", AtomToken "O", NumberToken 4]
+-- ((H2)(S1)(O4))
+--
 parse :: [Token] -> Compound
 parse tokens = parseCompound [Group [] 1] tokens
     where
@@ -73,11 +82,87 @@ parse tokens = parseCompound [Group [] 1] tokens
         parseCompound ((Group g1s _):(Group g2s n2):cs) (RParenToken:(NumberToken gn):ts)    = parseCompound ((Group (g2s ++ [Group g1s gn]) n2):cs) ts
         parseCompound ((Group g1s n1):(Group g2s n2):cs) (RParenToken:ts)                    = parseCompound ((Group (g2s ++ [Group g1s n1]) n2):cs) ts
 
+        parseCompound _ (t:_) = error ("unexpected token " ++ (show t))
         parseCompound _ _ = error "error"
 
--- >>> parse [AtomToken "H", NumberToken 2, AtomToken "O", NumberToken 1]
--- (O1)(H2)
+
+type ElementInfo = (String, String, String)
+
+elements :: [ElementInfo]
+elements = [("H", "hydrogen", "hydrogenium"), ("He", "helium", "helium"), ("S", "sulfur", "sulphur"), ("Na", "sodium", "natrium"), ("Fe", "iron", "ferrum")]
+
+elementName :: ElementInfo -> String
+elementName (_, name, _) = name
+
+elementSymbol :: ElementInfo -> String
+elementSymbol (symbol, _, _) = symbol
+
+elementLatinName :: ElementInfo -> String
+elementLatinName (_, _, latin) = latin
+
+-- >>> getElementBySymbol "He"
+-- ("He","helium")
 --
--- >>> parse [AtomToken "Na", LParenToken, AtomToken "O", AtomToken "H", RParenToken, NumberToken 2]
--- (Na1)((O1)(H1))2
+getElementBySymbol :: String -> ElementInfo
+getElementBySymbol symbol = search symbol elements
+    where
+        search :: String -> [ElementInfo] -> ElementInfo
+        search sym (e:es)
+            | sym == (elementSymbol e)  = e
+            | otherwise                 = search sym es
+        search sym [] = error ("element" ++ sym ++ "not found")
+
+-- list of element suffixes from section IR-5.3.3.2
+elementSuffixes :: [String]
+elementSuffixes = ["ogen", "ygen", "orus", "ese", "ine", "ium", "ic", "en", "on", "um", "ur", "y"]
+
+-- >>> indexOf 'l' "hello"
+-- 2
 --
+indexOf :: Eq a => a -> [a] -> Int
+indexOf x xs = index 0 x xs
+    where
+        index :: Eq a => Int -> a -> [a] -> Int
+        index _ _ [] = -1
+        index n y (x:xs)
+            | y == x    = n
+            | otherwise = index (n+1) y xs
+
+-- >>> getStem "oxygen"
+-- "ox"
+--
+-- >>> getStem . elementName . getElementBySymbol $ "Na"
+-- "sod"
+--
+getStem :: String -> String
+getStem name = take (findSuffixIndex name elementSuffixes) name
+    where
+        findSuffixIndex :: String -> [String] -> Int
+        findSuffixIndex name (e:es) = if i /= -1 then i else findSuffixIndex name es
+            where i = indexOf e (suffixes name)
+        findSuffixIndex name [] = length name
+
+        suffixes :: [a] -> [[a]]
+        suffixes []         = [] : []
+        suffixes f@(_:t) = f: suffixes t 
+
+
+-- "Exceptions include Zn and Group 18 elements ending in 'on', where the 'ide' ending is added to the element names"
+-- "For some elements (e.g. Fe, Ag, Au) a Latin stem is used before the 'ide' ending"
+-- >>> homoatomicAnionName "S"
+-- "sulfide"
+--
+-- >>> homoatomicAnionName "Fe"
+-- "ferride"
+--
+homoatomicAnionName :: String -> String
+homoatomicAnionName symbol
+    | symbol == "Zn"                    = "zincide"
+    | symbol == "Ne"                    = "neonide"
+    | symbol == "Ar"                    = "argonide"
+    | symbol == "Kr"                    = "kryptonide"
+    | symbol == "Xn"                    = "xenonide"
+    | symbol == "Rn"                    = "radonide"
+    | symbol == "Og"                    = "oganessonide"
+    | elem symbol ["Fe", "Ag", "Au"]    = (getStem . elementLatinName . getElementBySymbol $ symbol) ++ "ide"
+    | otherwise                         = (getStem . elementName . getElementBySymbol $ symbol) ++ "ide"
---
+++ readme.md
@@ -0,0 +1,27 @@
+## Nomenclature of Inorganic Chemistry (IUPAC)
+
+Link to the [full text](https://iupac.org/wp-content/uploads/2016/07/Red_Book_2005.pdf)
+
+> ### IR-5.2 STOICHIOMETRIC NAMES OF ELEMENTS AND BINARY COMPOUNDS
+>
+> When constructing a stoichiometric name for a binary compound, one element is
+> designated as the electropositive constituent and the other the electronegative constituent.
+> The electropositive constituent is by convention the element that occurs last in the sequence
+> of Table VI\* and its name is the unmodified element name (Table I). The name of the
+> electronegative constituent is constructed by modifying the element name with the ending
+> ‘ide’, as explained in detail for monoatomic anions in Section IR-5.3.3.2. All element names
+> thus modified with the ‘ide’ ending are given in Table IX.
+> The stoichiometric name of the compound is then formed by combining the name of the
+> electropositive constituent, cited first, with that of the electronegative constituent, both
+> suitably qualified by any necessary multiplicative prefixes (‘mono’, ‘di’, ‘tri’, ‘tetra’, ‘penta’,
+> etc., given in Table IV). The multiplicative prefixes precede the names they multiply, and are
+> joined directly to them without spaces or hyphens. The final vowels of multiplicative prefixes
+> should not be elided (although ‘monoxide’, rather than ‘monooxide’, is an allowed exception
+> because of general usage). The two parts of the name are separated by a space in English.
+
+> ### IR-5.3.3.2 Monoatomic anions
+>
+> The name ofamonoatomic anion is the element name (Table I) modified so as to carry the
+> anion designator ‘ide’, either formed by replacing the ending of the element name (‘en’,
+> ‘ese’, ‘ic’, ‘ine’, ‘ium’, ‘ogen’, ‘on’, ‘orus’, ‘um’, ‘ur’, ‘y’ or ‘ygen’) by ‘ide’ or by directly
+> adding ‘ide’ as an ending to the element name.
